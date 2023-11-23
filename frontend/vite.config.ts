fa5c254b259cf8337664981466dff383d696f59f
Chris Hildebrand <hildebrandc94@gmail.com>
2023-11-23 07:19:41 -0500

    Set proxy for deployment.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: "https://micro-reddit-backend.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
