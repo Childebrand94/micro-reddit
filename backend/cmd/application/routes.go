1b63eb8eb9d55386a859b56d79c9f2b7721bbccc
Chris Hildebrand <hildebrandc94@gmail.com>
2023-11-24 13:47:17 -0500

    feat: Added cross origin cors.

--- backend/cmd/application/routes.go
+++ backend/cmd/application/routes.go
@@ -16,6 +16,7 @@ func (a *App) loadRoutes() {
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
+		AllowedOrigins:   []string{"http://localhost:5173"},
 		AllowCredentials: true,
 		MaxAge:           300,
 	})
