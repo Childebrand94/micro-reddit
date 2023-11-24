2be24b8199637daacda96840e64706d0adf48cf5
Chris Hildebrand <hildebrandc94@gmail.com>
2023-11-23 19:20:43 -0500

    Created environment variables.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -37,6 +37,7 @@ function App() {
         }
     };
 
+
     useEffect(() => {
         fetchPosts(filter);
     }, [updateTrigger]);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+const apiBaseUrl = process.env.VITE_API_URL || 'http://127.0.0.1:3000'
+
+console.log(process.env.VITE_API_URL)
+
 export default defineConfig({
   plugins: [react()],
+  base : '/',
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: apiBaseUrl,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
